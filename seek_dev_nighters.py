6a290642a1ba4a035a556efecc90c3e0538196cb
Olekciy <0lekciy4@gmail.com>
2018-04-11 23:06:33 +0300

    fixed remarks

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -11,24 +11,30 @@ def load_api_page_data(url, page=1):
     return api_page_data
 
 
-def load_attempts(api_url_page):
-    pages = load_api_page_data(api_url_page)['number_of_pages']
-    for page in range(pages):
-        attempts = load_api_page_data(api_url_page, page + 1)['records']
+def load_attempts(api_url):
+    page = 1
+    while True:
+        page_data = load_api_page_data(api_url, page)
+        pages = page_data['number_of_pages']
+        attempts = page_data['records']
         for attempt in attempts:
             yield {
                 'username': attempt['username'],
                 'timestamp': attempt['timestamp'],
                 'timezone': attempt['timezone'],
             }
+        page += 1
+        if page > pages:
+            break
 
 
-def get_midnighters(get_attempt, morning=5):
+def get_midnighters(attempts, morning=5):
     midnighters = []
-    for attempt in get_attempt:
+    for attempt in attempts:
         attempt_timezone = pytz.timezone(attempt['timezone'])
-        attempt_time = attempt_timezone.localize(
-            datetime.fromtimestamp(attempt['timestamp'])
+        attempt_time = datetime.fromtimestamp(
+            attempt['timestamp'],
+            attempt_timezone,
         )
         if attempt_time.hour < morning:
             midnighters.append(attempt['username'])
@@ -36,9 +42,9 @@ def get_midnighters(get_attempt, morning=5):
 
 
 def main():
-    api_url_page = 'http://devman.org/api/challenges/solution_attempts/'
-    get_attempt = load_attempts(api_url_page)
-    midnighters = get_midnighters(get_attempt)
+    api_url = 'http://devman.org/api/challenges/solution_attempts/'
+    attempts = load_attempts(api_url)
+    midnighters = get_midnighters(attempts)
     for midnighter in midnighters:
         print(midnighter)
 
